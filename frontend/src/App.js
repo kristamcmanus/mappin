6e07140fcb4f43e3afcf10e0bdabd90a57821246
kristamcmanus <k.mcmanus310@gmail.com>
2022-02-21 18:26:29 -0500

    Install Mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import * as React from 'react';
+import Map from 'react-map-gl';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
 
