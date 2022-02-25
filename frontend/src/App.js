4ab0a7753f8723e56ee0c2d72a0bda0fb7b5f6ff
kristamcmanus <k.mcmanus310@gmail.com>
2022-02-25 11:31:18 -0500

    Add routes for pins and users as well as basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
 import * as React from 'react';
-import Map, {Marker} from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import StarIcon from '@mui/icons-material/Star';
+import "./App.css";
 // import { LocationOnIcon } from 'mapbox-gl/dist/mapbox-gl.css';
 // import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function App() {
   return (
     <div className="App">
-    <Map
+      <Map
         initialViewState={{
-        longitude: -77.03637,
-        latitude: 38.89511,
-        zoom: 14
-      }}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+          longitude: -77.036560,
+          latitude: 38.897957,
+          zoom: 14
+        }}
+        style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
+        mapStyle="mapbox://styles/kmcmanus310/ckzxefk7x000y14nqjbjx360f"
       >
-        <Marker longitude={-100} latitude={40} anchor="bottom" >
+        <Marker longitude={-77.036560} latitude={38.897957} anchor="bottom" >
           {/* <img src="./pin.png" alt="" /> */}
-          <LocationOnIcon style={{ fontSize: "40" }} />
+          <LocationOnIcon style={{ fontSize: 40, color: "black" }} />
         </Marker>
+        <Popup
+          longitude={-77.036560}
+          latitude={38.897957}
+          anchor="bottom"
+        >
+          <div className='card'>
+            <label>Place</label>
+            <h4 className='place'>White House</h4>
+            <label>Review</label>
+            <p className='desc'>Beautiful place. I like it.</p>
+            <label>Rating</label>
+            <div className='stars'>
+              <StarIcon className='star' />
+              <StarIcon className='star' />
+              <StarIcon className='star' />
+              <StarIcon className='star' />
+              <StarIcon className='star' />
+            </div>
+            <label>Information</label>
+            <span className='username'>Created by <b>Krista</b></span>
+            <span className='date'>1 hour ago</span>
+          </div>
+          You are here
+        </Popup>
       </Map>
     </div>
   );
@@ -44,13 +70,13 @@ export default App;
 //         {...viewport}
 //         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
 //         onViewportChange={nextViewport => setViewport(nextViewport)}
-//       >  
+//       >
 //         <Marker
 //           latitude={37.78}
 //           longitude={-122.41}
 //           offsetLeft={-20}
 //           offsetTop={-10}
-//         >  
+//         >
 
 //           <div>You are here</div>
 //         </Marker>
